d08369b0d9b0b84ee5e1273f27256a98d10de95a
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-09 10:53:19 -0500

    fix broken solution

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -74,8 +74,8 @@ describe "CLI Jukebox" do
 
     it "responds to 'exit'" do
       self.stub(:gets).and_return("exit")
-      help_output = capture_stdout { run(songs) }
-      expect(help_output).to include("Goodbye")
+      exit_output = capture_stdout { run(songs) }
+      expect(exit_output).to include("Goodbye")
     end
   end
 end
